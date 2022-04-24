5f58c7e0af8bcbfada79ac399d0857387aa67f53
Nathan <github@graphicalphabet.com>
2022-04-24 15:26:34 +0900

    Added Self-Love Mantras

---
+++ js/mantras.js
@@ -0,0 +1,93 @@
+const mantras = [{
+    mantra: 'Things are always working out for me.'
+}, {
+    mantra: 'I accomplish everything I set my mind to.'
+}, {
+    mantra: 'I deserve and choose to be happy.'
+}, {
+    mantra: 'I am always at the right place at the right time.'
+}, {
+    mantra: 'I am loved for who I am, not what I do.'
+}, {
+    mantra: 'I am beautiful just as I am.'
+}, {
+    mantra: 'I am amazing.'
+}, {
+    mantra: 'I have something special to offer the world.'
+}, {
+    mantra: 'I am worthy of love.'
+}, {
+    mantra: 'I feel safe and secure in the world.'
+}, {
+    mantra: 'I have deep inner strength.'
+}, {
+    mantra: 'I matter.'
+}, {
+    mantra: 'I celebrate everything in my life.'
+}, {
+    mantra: 'Everything that I need is inside me now.'
+}, {
+    mantra: 'I am enough, there is enough, I need no more.'
+}, {
+    mantra: 'I am strong. I can do this.'
+}, {
+    mantra: 'I am learning to trust my own decisions.'
+}, {
+    mantra: 'I give myself permission to take risks.'
+}, {
+    mantra: 'I am full of gratitude.'
+}, {
+    mantra: 'My joy is my greatest gift.'
+}, {
+    mantra: 'All my needs are met above and beyond my greatest expectations.'
+}, {
+    mantra: 'My life is divinely guided.'
+}, {
+    mantra: 'I forgive myself.'
+}, {
+    mantra: 'I create my happiness by loving myself unconditionally.'
+}, {
+    mantra: 'I experience joy in everything I do.'
+}, {
+    mantra: 'My potential is endless.'
+}, {
+    mantra: 'I love who I am becoming.'
+}, {
+    mantra: 'Creativity is flowing through me.'
+}, {
+    mantra: 'I have what it takes to fulfill my desires.'
+}, {
+    mantra: 'I believe in myself.'
+}, {
+    mantra: 'I have what it takes to succeed.'
+}, {
+    mantra: 'Money is easily flowing into my life.'
+}, {
+    mantra: 'I have all the money that I need at this moment.'
+}, {
+    mantra: 'I release all thoughts of what I lack and focus on what I have.'
+}, {
+    mantra: 'My life is an adventure.'
+}, {
+    mantra: 'I accept and cherish myself as I am.'
+}, {
+    mantra: 'I am profoundly beautiful.'
+}, {
+    mantra: 'I am the creator of my own life.'
+}, {
+    mantra: 'I am open to hear what my inner voice is whispering to me.'
+}, {
+    mantra: 'I have the courage to follow my inner wisdom.'
+}, {
+    mantra: 'It is safe for me to listen to my intuition.'
+}, {
+    mantra: 'My intuition is my inner compass, guiding my actions.'
+}, {
+    mantra: 'I am attracting abundance in my life.'
+}, {
+    mantra: 'I am pure bliss.'
+}, {
+    mantra: 'I accept, honor, and thank my past for helping me get here.'
+}, {
+    mantra: 'My higher self is my best guide.'
+}];
